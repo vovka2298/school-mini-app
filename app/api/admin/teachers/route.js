5ee2a3408b0f86eb75cca62292688dd5839ae12d
vovka2298 <91417467+vovka2298@users.noreply.github.com>
2025-12-10 14:56:51 +0300

    Update route.js

--- app/api/admin/teachers/route.js
+++ app/api/admin/teachers/route.js
@@ -1,4 +1,4 @@
-import { getAllTeachers, getTeacherStudents } from '../redis-client.js';
+import { getAllTeachers, getTeacherStudents } from '../../redis-client.js';
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
