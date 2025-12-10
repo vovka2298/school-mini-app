bf314bc2af43ec33a9e71f92417f4fa1d14fb6ba
vovka2298 <91417467+vovka2298@users.noreply.github.com>
2025-12-10 14:56:15 +0300

    Update route.js

--- app/api/admin/stats/route.js
+++ app/api/admin/stats/route.js
@@ -1,4 +1,4 @@
-import { getSystemStats } from '../redis-client.js';
+import { getSystemStats } from '../../redis-client.js';
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
